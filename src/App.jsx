026a848dc9fe5be42f01f94d92e65685e6a828e5
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-02-19 09:52:34 +0530

    fix:

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,10 @@ import {} from "react";
 import Todo from "./Components/Todo";
 
 function App() {
-  const todos = [
-    { data: "todo1", id: 312 },
-    { data: "todo2", id: 313 },
-    { data: "todo3", id: 213 },
-  ];
   return (
     <>
       <main>
-        <Todo todoList={todos} />
+        <Todo />
       </main>
     </>
   );
